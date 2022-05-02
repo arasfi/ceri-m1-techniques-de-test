3abe435d73040872b8fa4ff659c2e5f3d04c8452
Abdelhakim RASFI <abdelhakim.rasfi@univ-avignon.fr>
2022-05-02 12:59:36 +0200

    Added: Pom.xml And AppTest

---
+++ src/main/java/fr/univavignon/pokedex/api/App.java
@@ -0,0 +1,13 @@
+package fr.univavignon.pokedex.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
